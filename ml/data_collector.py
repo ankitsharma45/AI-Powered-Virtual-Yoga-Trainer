1f3d0e03088d1fd478bb25ccbe156feff66676b6
Ankit Sharma <ankitsharmaji4545@gmail.com>
2026-07-05 01:05:38 +0530

    Integrated MediaPipe and Draw Skeleton

--- ml/data_collector.py
+++ ml/data_collector.py
@@ -27,28 +27,46 @@ import mediapipe as mp
 # MediaPipe Pose Shortcut
 mp_pose = mp.solutions.pose
 
+pose = mp_pose.Pose(
+    static_image_mode=False,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5
+)
+
 # Drawing Utility
 mp_draw = mp.solutions.drawing_utils
 
 
-cap=cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 
-if not cap.isOpened:
-    print("Error : Unable to open camera")
+if not cap.isOpened():
+    print("Error: Unable to open camera")
     exit()
 
 while True:
-    sucess,frame = cap.read()
+    success, frame = cap.read()
 
-    if not sucess:
+    if not success:
         print("Error: Frame not read")
         break
-    cv2.imshow("AI-Powered Virtual Yoga Trainer - Collect Dataset for Training", frame)
+
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = pose.process(rgb_frame)
+
+    if results.pose_landmarks:
+
+        mp_draw.draw_landmarks(
+                frame,
+                results.pose_landmarks,
+                mp_pose.POSE_CONNECTIONS
+        )
+
+
+
+    cv2.imshow("AI-Powered Virtual Yoga Trainer - Dataset Collector", frame)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
     
 cap.release()
 cv2.destroyAllWindows()
-
-# if __name__ == "__main__":

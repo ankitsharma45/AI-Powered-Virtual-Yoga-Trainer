9d27586dc153645803efd10034f5646a691547fb
Ankit Sharma <ankitsharmaji4545@gmail.com>
2026-07-05 00:10:49 +0530

    Sync data collector from main

--- ml/data_collector.py
+++ ml/data_collector.py
@@ -29,3 +29,26 @@ mp_pose = mp.solutions.pose
 
 # Drawing Utility
 mp_draw = mp.solutions.drawing_utils
+
+
+cap=cv2.VideoCapture(0)
+
+if not cap.isOpened:
+    print("Error : Unable to open camera")
+    exit()
+
+while True:
+    sucess,frame = cap.read()
+
+    if not sucess:
+        print("Error: Frame not read")
+        break
+    cv2.imshow("AI-Powered Virtual Yoga Trainer - Collect Dataset for Training", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
+
+# if __name__ == "__main__":
